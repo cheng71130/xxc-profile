7287ca1aad6a892a8684ffc770d74bf14dbdfa7a
xxc <cheng71130@gmail.com>
2025-12-26 16:37:14 +0800

    解决生产环境一直loading的问题

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-	<div id="app">
-		<router-view />
-	</div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    onMounted(() => {
+        setTimeout(() => {
+            const loading = document.getElementById('loading-screen');
+            if (loading) {
+                loading.remove();
+            }
+        }, 300);
+    });
+</script>
 <style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router/index';
 import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
-// import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import SvgIcon from './components/SvgIcon/index.vue';
 import DataVVue3 from '@kjgl77/datav-vue3';
 import elegantLoading from './directives/elegantLoading';
@@ -16,11 +15,6 @@ const app = createApp(App);
 
 // 全局组件
 app.component('SvgIcon', SvgIcon);
-
-// 注册所有el-icon图标
-// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-//     app.component(key, component);
-// }
 app.use(ElementPlus, { locale: zhCn });
 app.use(router);
 app.use(DataVVue3);
